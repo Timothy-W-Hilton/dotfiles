730f8911cd36b69c5af72a091c2c8fbe0673f169
Timothy W. Hilton <twhilton@ucsc.edu>
2021-01-27 11:13:26 -0700

    remove superfluous semicolons

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,10 @@
 # pilfered from https://github.com/mathiasbynens/dotfiles/blob/main/bootstrap.sh
 # -- TWH, 22 June 2020
 
-cd "$(dirname "${BASH_SOURCE}")";
+cd "$(dirname "${BASH_SOURCE}")"
 
-git pull origin master;
-git -C $HOME/Software/dircolors-solarized pull origin master;
+#git pull origin master
+git -C $HOME/Software/dircolors-solarized pull origin master
 
 function doIt() {
 	rsync --exclude ".git/" \
@@ -15,17 +15,17 @@ function doIt() {
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
+		-avh --no-perms . ~
+	source ~/.bash_profile
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+if [ "$1"=="--force" -o "$1"=="-f" ] then
+   doIt
+else 
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+    echo ""
+    if [[ $REPLY =~ ^[Yy]$ ]] then
+       doIt
+    fi
+fi
+unset doIt
