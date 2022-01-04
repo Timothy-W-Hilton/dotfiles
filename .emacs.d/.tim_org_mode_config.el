332ba1696fbd36cc8937febad3aed9e0de37aedd
Timothy W. Hilton <t.hilton@gns.cri.nz>
2022-01-05 12:09:10 +1300

    org mode: bind C-a to org agenda; set deadline warning to 60 days

--- .emacs.d/.tim_org_mode_config.el
+++ .emacs.d/.tim_org_mode_config.el
@@ -1,5 +1,8 @@
 ;;--------------------------------------------------
 ;; org mode
+(global-set-key "\C-ca" 'org-agenda)
+(setq-local org-deadline-warning-days 60)
+
 (add-hook 'inferior-org-mode-hook
           (lambda()
 	    (local-set-key (kbd "M-q") 'org-fill-paragraph)))
