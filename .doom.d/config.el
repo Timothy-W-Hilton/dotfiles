24449b29bd321e83e253cb81f8f8932fa5601c5d
Timothy W. Hilton <t.hilton@gns.cri.nz>
2022-09-19 15:33:00 +1200

    go back to zenburn theme]

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -34,8 +34,9 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;;(setq doom-theme 'doom-one)  ; default
-(setq doom-theme 'doom-zenburn)  ; default
+;; (setq doom-theme 'doom-one)  ; default
+(setq doom-theme 'doom-zenburn)  ; the python interpreter error face is awful :(
+;;(setq doom-theme 'doom-city-lights)                                 ;
                              ;
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
